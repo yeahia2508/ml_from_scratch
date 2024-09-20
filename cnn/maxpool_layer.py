5af153632c95ccb779dfe12692116d91b03c4b87
Yeahia Md Arif <yeahia.arif@gmail.com>
2024-09-20 17:43:33 +0600

    implement and test backward propagation in maxpool layer

--- cnn/maxpool_layer.py
+++ cnn/maxpool_layer.py
@@ -4,9 +4,6 @@ import numpy as np
 class MaxPoolLayer:
         
     def forward(self, imageMatrix, kernelShape = [2,2], stride = 1):
-        self.inputs = imageMatrix
-        self.kernelShape = kernelShape
-        self.stride = stride
         
         [imageHeight, imageWidth, colorChannelSize, batchSize] = imageMatrix.shape
         
@@ -18,12 +15,18 @@ class MaxPoolLayer:
         
         output = np.zeros((xOutput, yOutput, colorChannelSize, batchSize))
         
-        image_matrix_copy = imageMatrix.copy() * 0
+        image_matrix_copy = imageMatrix.copy().astype(float) * 0
+        
+        storeMaxIndexs = np.zeros((kernelHeight * kernelWidth, 2, xOutput * yOutput, colorChannelSize, batchSize))
+        storeNMax = np.zeros((xOutput*yOutput, colorChannelSize, batchSize))
         
         for i in range(batchSize):
             for c in range(colorChannelSize):
+                window_index = 0
                 for w in range(xOutput):
                     for h in range(yOutput):
+                        window_index += 1
+                        
                         xStart = h * stride
                         xEnd = xStart + kernelHeight
                         yStart = w * stride
@@ -33,12 +36,49 @@ class MaxPoolLayer:
                         max_value = float(currentSlice.max())
                         output[h, w, c , i] = max_value
                         
+                        (x_max_indexs, y_max_indexs) = np.where(currentSlice == max_value)
+                        
+                        for ii, (xx,yy) in enumerate(zip(x_max_indexs, y_max_indexs)):
+                            storeMaxIndexs[ii, 0, window_index -1, c, i] = xx
+                            storeMaxIndexs[ii, 1, window_index -1, c, i] = yy
+                            
+                        storeNMax[window_index - 1, c, i] = ii + 1
                         image_matrix_copy[xStart:xEnd, yStart:yEnd, c, i] += np.equal(imageMatrix[xStart:xEnd, yStart:yEnd, c, i], max_value).astype(float)
+        
         
         mask = image_matrix_copy
         mask = np.matrix.round(mask/(mask + 1e-7))
         
         self.output = output
-
         self.mask = mask
+        self.maxIndexs = storeMaxIndexs
+        self.windowMaxCount = storeNMax
+        self.kernelShape = kernelShape
+        self.stride = stride
+        self.input = imageMatrix
+        
+    def backward(self, dvalues):
+        [dx, dy, cs, bs] = dvalues.shape
+        stride = self.stride
+        dinputs = np.zeros(self.input.shape)
+        
+        for i in range(bs):
+            for c in range(cs):
+                window_index = 0
+                for y in range(dy):
+                    for x in range(dx):
+                        window_index += 1
+                        yStart = y * stride
+                        xStart = x * stride
+                        max_count = int(self.windowMaxCount[window_index - 1, c , i])
+                        
+                        for ii in range(max_count):
+                            xx = int(self.maxIndexs[ii, 0, window_index -1, c, i])
+                            yy = int(self.maxIndexs[ii, 1, window_index -1, c, i])
+                            
+                            dinputs[xStart + xx, yStart + yy, c, i] = dvalues[x, y, c, i]
+                            
+        self.dinputs = dinputs
+        
+        
 
--- cnn/test/max_pool_test.py
+++ cnn/test/max_pool_test.py
@@ -1,27 +1,30 @@
 # -*- coding: utf-8 -*-
-
 from cnn.utils import *
 from cnn.conv_layer import *
 from cnn.maxpool_layer import *
 import matplotlib.pyplot as plt
 
+
 readImage = ReadImageClass()
-random_classes, random_image_matrix = readImage.get_batch_image_with_class('../images', 2, [120,120])
+random_classes, random_image_matrix = readImage.get_batch_image_with_class(
+    '../images', 2, [120, 120])
 convLayer1 = ConvLayer([3,3], 5)
 maxpool1 = MaxPoolLayer()
-convLayer2 = ConvLayer([5,5], 8)
 
 convLayer1.forward(random_image_matrix, padding = 2, stride = 1)
-maxpool1.forward(convLayer1.output, stride = 1)
+maxpool1.forward(random_image_matrix, stride = 2)
+maxpool1.backward(maxpool1.output)
 
 im1 = convLayer1.output[:, : , 0, 0]
 im2 = maxpool1.output[:, :, 0, 0]
 im3 = maxpool1.mask[:, :, 0, 0]
-# convLayer2.forward(convLayer1.output, padding = 1, stride = 2)
+im4 = maxpool1.dinputs[:, :, 0, 0]
 
 plt.imshow(im1)
 plt.show()
 plt.imshow(im2)
 plt.show()
 plt.imshow(im3)
+plt.show()
+plt.imshow(im4)
 plt.show()
