e662d6faa6da8461056b8c1c7e06bd844557bed8
Yeahia Md Arif <yeahia.arif@gmail.com>
2024-09-26 22:44:54 +0600

    Add l2 regularization in dense layer

--- ann/layer_dense.py
+++ ann/layer_dense.py
@@ -10,6 +10,10 @@ class Layer_Dense:
         # Initialize weights and biases
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+        
+        L2 = 1e-2
+        self.weights_L2 = L2
+        self.biases_L2  = L2
 
     # Forward pass
     def forward(self, inputs):
@@ -25,4 +29,7 @@ class Layer_Dense:
         self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
         # Gradient on values
         self.dinputs = np.dot(dvalues, self.weights.T)
+        
+        self.dbiases  = self.dbiases  + 2* self.biases_L2 *self.biases
+        self.dweights = self.dweights + 2* self.weights_L2 *self.weights
 
