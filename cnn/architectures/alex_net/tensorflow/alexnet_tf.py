122386b20001dd67a311118c819f3194b2958b1e
Yeahia Md Arif <yeahia.arif@gmail.com>
2024-10-03 23:49:57 +0600

    add regularaization in alexnet python file

--- cnn/architectures/alex_net/tensorflow/alexnet_tf.py
+++ cnn/architectures/alex_net/tensorflow/alexnet_tf.py
@@ -8,8 +8,8 @@ Created on Sun Sep 29 21:10:26 2024
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.losses import categorical_crossentropy
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, ZeroPadding2D, Dropout
-
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, ZeroPadding2D, Dropout, Input
+from tensorflow.keras import regularizers
 from tensorflow.keras.utils import to_categorical
 from keras.datasets import mnist
 from tensorflow.keras.datasets import cifar10
@@ -23,27 +23,28 @@ from utils.callbacks import TfCallback
 import datetime
 
 class AlexNet(Sequential):
-    def __init__(self, input_shape, nb_classes):
+    def __init__(self, input_shape, nb_classes, l2 = 0.001):
         super().__init__()
-        self.add(Conv2D(96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape= input_shape, padding='valid'))
+        self.add(Input(shape=input_shape)),
+        self.add(Conv2D(96, kernel_size=(11,11), strides=(4,4), activation='relu', padding='valid', kernel_regularizer=regularizers.l2(l2)))
         self.add(MaxPooling2D(pool_size=(3,3), strides=(2,2), padding='valid'))
         self.add(ZeroPadding2D(padding=2))
-        self.add(Conv2D(256, kernel_size=(5,5), strides=(1,1), activation='relu'))
+        self.add(Conv2D(256, kernel_size=(5,5), strides=(1,1), activation='relu', kernel_regularizer=regularizers.l2(0.01)))
         self.add(MaxPooling2D(pool_size=(3,3), strides=(2,2), padding='valid'))
-        self.add(Conv2D(384, kernel_size=(3,3), strides=(1,1), activation='relu', padding='same'))
-        self.add(Conv2D(384, kernel_size=(3,3), strides=(1,1), activation='relu', padding='same'))
-        self.add(Conv2D(256, kernel_size=(3,3), strides=(1,1), activation='relu', padding='same'))
+        self.add(Conv2D(384, kernel_size=(3,3), strides=(1,1), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2)))
+        self.add(Conv2D(384, kernel_size=(3,3), strides=(1,1), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2)))
+        self.add(Conv2D(256, kernel_size=(3,3), strides=(1,1), activation='relu', padding='same',kernel_regularizer=regularizers.l2(l2)))
         self.add(Flatten())
-        self.add(Dense(4096, activation='relu'))
+        self.add(Dense(4096, activation='relu', kernel_regularizer=regularizers.l2(l2)))
         self.add(Dropout(0.5))
-        self.add(Dense(4096, activation='relu'))
+        self.add(Dense(4096, activation='relu', kernel_regularizer=regularizers.l2(l2)))
         self.add(Dropout(0.5))
         self.add(Dense(nb_classes, activation='softmax'))
         
         lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-            initial_learning_rate = 1e-2,
-            decay_steps = 10000,
-            decay_rate = 0.98
+            initial_learning_rate = 1e-4,
+            decay_steps = 875,
+            decay_rate = 0.95
         )
         
         adam = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
