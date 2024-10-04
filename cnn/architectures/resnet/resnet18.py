fa41d13b2f1e759668dfecdbd1db3989ee03a5f3
Yeahia Md Arif <yeahia.arif@gmail.com>
2024-10-04 21:56:30 +0600

    init resnet cnn architecture

---
+++ cnn/architectures/resnet/resnet18.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  4 21:55:19 2024
+
+@author: yeahia
+"""
+
+class Resnet18:
+    def __init__():
+        print('hello resnet18')
