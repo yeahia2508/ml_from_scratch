ff171d8fafd7f11bb785aab7aa582070da9f87e2
Yeahia Md Arif <yeahia.arif@gmail.com>
2024-10-12 22:44:32 +0600

    add python file extension for mobilenet

---
+++ cnn/architectures/mobilenetV3/mobilenet.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 12 22:39:28 2024
+
+@author: yeahia
+"""
+
+class MobileNetV3:
+    def __init__():
+        print("Hello World")
