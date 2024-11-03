d03ce57b569d8b4b664a0fc23f424ebb6441d4f4
Yeahia Md Arif <yeahia.arif@gmail.com>
2024-11-03 23:34:03 +0600

    implemented yolo loss function

--- object_detection/model/yolo/yolo_v1/loss.py
+++ object_detection/model/yolo/yolo_v1/loss.py
@@ -25,3 +25,53 @@ class YoloLoss(nn.Module):
         iou_maxes, bestbox = torch.max(ious, dim = 0)
         exists_box = target[...,20].unsqueeze(3)
         
+        box_predictions = exists_box * ((bestbox * predictions[..., 26: 30]) + (1 - bestbox) * predictions[..., 21:25])
+        box_targets = exists_box * target[..., 21:25]
+        
+        
+        #box corrdinate loss
+        
+        box_predictions[..., 2:4] = torch.sign(box_predictions[..., 2:4]) * torch.sqrt(torch.abs(box_predictions[..., 2:4]+ 1e-6))
+        box_targets[..., 2:4] =  torch.sqrt(box_targets[..., 2:4])
+        box_loss = self.mse(
+            torch.flatten(box_predictions, end_dim=-2), 
+            torch.flatten(box_targets, end_dim=-2),
+        )
+        
+        #Confidence Object loss
+        
+        pred_box = (bestbox * predictions[..., 25:26] + (1 - bestbox) * predictions[..., 20:21])
+        object_loss = self.mse(
+            torch.flatten(exists_box * pred_box),
+            torch.flatten(exists_box * target[..., 20:21])
+        )
+        
+        #Confidence No Object loss
+        
+        no_object_loss = self.mse(
+            torch.flatten((1 - exists_box) * predictions[..., 20:21], start_dim = 1 ),
+            torch.flatten((1 - exists_box) * target[..., 20:21], start_dim = 1)
+        )
+        
+        no_object_loss += self.mse(
+            torch.flatten((1 - exists_box) * predictions[..., 25:26], start_dim = 1),
+            torch.flatten((1 - exists_box) * target[..., 20:21], start_dim = 1)
+        )
+        
+        # class loss
+        class_loss = self.mse(
+            torch.flatten(exists_box * predictions[..., :20], end_dim=-2),
+            torch.flatten(exists_box * target[..., 20], end_dim=-2)
+        )
+        
+        loss = (
+            self.lambda_coord * box_loss + 
+            object_loss +
+            self.lambda_noobj * no_object_loss +
+            class_loss
+        )
+        
+        return loss
+        
+        
+        
