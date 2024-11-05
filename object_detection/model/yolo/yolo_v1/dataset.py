2fc1473ade82a55831b5e280ebbbd8a3d0a3cb54
Yeahia Md Arif <yeahia.arif@gmail.com>
2024-11-05 23:56:33 +0600

    implement voc dataset class for yolo model

--- object_detection/model/yolo/yolo_v1/dataset.py
+++ object_detection/model/yolo/yolo_v1/dataset.py
@@ -37,3 +37,20 @@ class VOCDataset(torch.utils.data.Dataset):
         if self.transform:
             image, boxes = self.transform(image, boxes)
         label_matrix = torch.zeros((self.S, self.S, self.C + 5 * self.B))
+        for box in boxes:
+            class_label, x, y, width, height = box.tolist()
+            class_label = int(class_label)
+            
+            i,j = int(self.S * y), int(self.S * x)
+            x_cell, y_cell = self.S * x - j, self.S * y - i
+            
+            width_cell, height_cell = (width * self.S, height * self.S)
+            
+            if label_matrix[i,j,20] == 0:
+                label_matrix[i,j,20] = 1
+                box_coordinates = torch.tensor(
+                    [x_cell, y_cell, width_cell, height_cell]
+                )
+                label_matrix[i,j,21:25] = box_coordinates
+                
+                label_matrix[i,j,class_label] = 1
