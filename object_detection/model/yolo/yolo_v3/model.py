ae6c9b717d60a27a7933bcc082cddb1f3bcad708
Yeahia Md Arif <yeahia.arif@gmail.com>
2024-11-19 23:53:20 +0600

    implement cnn and residual block

--- object_detection/model/yolo/yolo_v3/model.py
+++ object_detection/model/yolo/yolo_v3/model.py
@@ -1,3 +1,6 @@
+import torch
+import torch.nn as nn
+
 config = [
     (32, 3, 1),
     (64, 3, 2),
@@ -24,3 +27,50 @@ config = [
     (256, 3, 1),
     "S",
 ]
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, bn_act=True, **kwargs):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias = not bn_act, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.leaky = nn.LeakyReLU(0.1)
+        self.use_bn_act = bn_act
+    
+    def forward(self, x):
+        if self.use_bn_act:
+            return self.leaky(self.bn(self.conv(x)))
+        else:
+            return self.conv(x)
+        
+        
+class ResidualBlock(nn.Module):
+    def __init__(self, channels, use_residual=True, num_repeats=1):
+        super().__init__()
+        self.layers = nn.ModuleList()
+        for repeat in range(num_repeats):
+            self.layers += [
+                nn.Sequential(
+                    CNNBlock(channels, channels // 2, kernel_size=1),
+                    CNNBlock(channels, channels // 2, kernel_size=3, padding=1)
+                )
+            ]
+        self.use_residual = use_residual
+        self.num_repeats = num_repeats
+
+    def forward(self, x):
+        for layer in self.layers:
+            if self.use_residual:
+                x = x + layer(x)
+            else:
+                x = layer(x)
+
+        return x
+
+class ScalePrediction(nn.Module):
+    pass
+
+class YOLOv3(nn.Module):
+    pass
+
+        
+    
